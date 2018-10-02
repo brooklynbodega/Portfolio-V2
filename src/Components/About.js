6080154ba1fb2f57d7f67b98c9f4cf25599c213a
Raul Montero <raul.amontero@gmail.com>
2018-10-02 15:13:12 -0400

    selected display font

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,26 @@ import '../App.css';
 class About extends Component {
   render() {
     return (
-      <div className="About-Me">
-        <p>From MA, moved to BK.</p>
+      <div className="About">
+        <div id="Bio">
+         <p>As a burgeoning developer based in Brooklyn, NY, my passion is to create accessible applications with code that is composed and discernible. I enjoy strategizing paths to success and encourage constructive communication to produce work of the highest quality. My goal is to immerse myself with a brand that is intent on crafting innovative, dynamic programming with an egalitarian ethos.</p>
+        </div>
+        <div className="Contact">
+          <div id="social-links">
+            <a href="https://www.linkedin.com/in/raul-montero/">
+              <img src={ require("../images/linkedin-logo.svg")} alt="linkedin"/>
+            </a>
+            <a href="https://github.com/brooklynbodega">
+              <img src={ require("../images/github-logo.svg")} alt="github"/>
+            </a>
+            <a href="mailto:raul.amontero@gmail.com">
+              <img src={ require("../images/gmail-logo.svg")} alt="gmail"/>
+            <a href="https://www.facebook.com/montero.raul">
+              <img src={ require("../images/facebook-logo.svg")} alt="facebook"/>
+            </a>
+            </a>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,42 +4,36 @@ import '../App.css';
 class Projects extends Component {
   render() {
     return (
-      <div class="projects-wrapper">
-        <div class="projects" id="one">
-          <p>
+      <div className="projects-wrapper">
+        <div className="projects" id="project-one">
             <a href="https://brooklynbodega.github.io/Project-01/">
-              <img src={ require("../images/project-1-photo-00.png")} alt="project-1"/>
-              <div class="description">
+              <img className="project-image" id="proj-img-one" src={ require("../images/project-1-photo-00.png")} alt="project-1"/>
+              <div className="description">
               <h2>Cell Match</h2>
                 <p>A color-matching tile game that cycles through various colors within a set timeframe with increasing difficulty. Written in Javascript with transitions used from Animista.net.
                 </p>
               </div>
             </a>
-          </p>
         </div>
-        <div class="projects" id="two">
-          <p>
+        <div className="projects" id="project-two">
             <a href="https://project-2-volunteer.herokuapp.com/">
-              <img src={require("../images/project-2-photo-00.png")} alt="project-2"/>
-              <div class="description">
+              <img className="project-image" id="proj-img-two" src={require("../images/project-2-photo-00.png")} alt="project-2"/>
+              <div className="description">
                 <h2>CommunityCares</h2>
                   <p>A CRUD app developed with Express and Node.js that allows users to create and share volunteer projects.
                   </p>
               </div>
             </a>
-          </p>
         </div>
-        <div class="projects" id="three">
-          <p>
+        <div className="projects" id="project-three">
             <a href="https://lovo.netlify.com/">
-              <img src={require("../images/project-4-photo-00.png")} alt="project-4"/>
-              <div class="description">
+              <img className="project-image" id="proj-img-three" src={require("../images/project-4-photo-00.png")} alt="project-4"/>
+              <div className="description">
                 <h2>LoVo</h2>
                   <p>A user-facing database to assist citizens in finding information on their direct representatives at all government levels using the ProPublica API, Google Civic API, and React.
                   </p>
               </div>
             </a>
-          </p>
         </div>
       </div>
     )
