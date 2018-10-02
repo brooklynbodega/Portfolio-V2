e33466a2e9ca2c4f7098fbb586b573262d122578
Raul Montero <raul.amontero@gmail.com>
2018-10-02 16:46:11 -0400

    hover feature functioning but improperly

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,32 +6,32 @@ class Projects extends Component {
     return (
       <div className="projects-wrapper">
         <div className="projects" id="project-one">
-            <a href="https://brooklynbodega.github.io/Project-01/">
+            <a className="image-container" href="https://brooklynbodega.github.io/Project-01/">
               <img className="project-image" id="proj-img-one" src={ require("../images/project-1-photo-00.png")} alt="project-1"/>
               <div className="description">
-              <h2>Cell Match</h2>
+                <h2>Cell Match</h2>
                 <p>A color-matching tile game that cycles through various colors within a set timeframe with increasing difficulty. Written in Javascript with transitions used from Animista.net.
                 </p>
               </div>
             </a>
         </div>
         <div className="projects" id="project-two">
-            <a href="https://project-2-volunteer.herokuapp.com/">
+            <a className="image-container" href="https://project-2-volunteer.herokuapp.com/">
               <img className="project-image" id="proj-img-two" src={require("../images/project-2-photo-00.png")} alt="project-2"/>
               <div className="description">
                 <h2>CommunityCares</h2>
-                  <p>A CRUD app developed with Express and Node.js that allows users to create and share volunteer projects.
-                  </p>
+                <p>A CRUD app developed with Express and Node.js that allows users to create and share volunteer projects.
+                </p>
               </div>
             </a>
         </div>
         <div className="projects" id="project-three">
-            <a href="https://lovo.netlify.com/">
+            <a className="image-container" href="https://lovo.netlify.com/">
               <img className="project-image" id="proj-img-three" src={require("../images/project-4-photo-00.png")} alt="project-4"/>
               <div className="description">
                 <h2>LoVo</h2>
-                  <p>A user-facing database to assist citizens in finding information on their direct representatives at all government levels using the ProPublica API, Google Civic API, and React.
-                  </p>
+                <p>A user-facing database to assist citizens in finding information on their direct representatives at all government levels using the ProPublica API, Google Civic API, and React.
+                </p>
               </div>
             </a>
         </div>
