b967bac2cbbea3e7d4d428efe6abafd36a8677b5
Raul Montero <raul.amontero@gmail.com>
2018-10-04 19:36:06 -0400

    added image and style to About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,25 +5,28 @@ class About extends Component {
   render() {
     return (
       <div className="About slide-in-blurred-right">
-        <div id="bio">
-         <p id="bio-text">
-          // Hello, I'm Raúl Montero. Previously a financial administrator and now a burgeoning developer based in Brooklyn, NY. Creating accessible applications with code that is composed and discernible is my modus operandi. I enjoy strategizing paths to success and encourage constructive communication to produce work of the highest quality. My goal is to immerse myself with a brand that is intent on crafting innovative, dynamic programming with an egalitarian ethos.
-         </p>
+        <div className="photo">
+          <img src = {require("../images/profile-photo-01.jpg")} alt="profile photo" title="profile photo" />
         </div>
-        <div className="contact">
-          <div id="social-links">
-            <a href="https://www.linkedin.com/in/raul-montero/">
-              <img src={ require("../images/linkedin-logo.svg")} alt="linkedin"/>
-            </a>
-            <a href="https://github.com/brooklynbodega">
-              <img src={ require("../images/github-logo.svg")} alt="github"/>
-            </a>
-            <a href="mailto:raul.amontero@gmail.com">
-              <img src={ require("../images/gmail-logo.svg")} alt="gmail"/>
-            <a href="https://www.facebook.com/montero.raul">
-              <img src={ require("../images/facebook-logo.svg")} alt="facebook"/>
-            </a>
-            </a>
+        <div id="bio">
+         <div id="bio-text">
+          Hello, I'm Raúl Montero. Previously a financial administrator turned burgeoning developer based in Brooklyn, NY. Creating accessible applications with code that is composed and discernible is my modus operandi. I enjoy strategizing paths to success and encourage constructive communication to produce work of the highest quality. My goal is to immerse myself with a brand that is intent on crafting innovative, dynamic programming with an egalitarian ethos.
+         </div>
+          <div className="contact">
+            <div id="social-links">
+              <a href="https://www.linkedin.com/in/raul-montero/">
+                <img src={ require("../images/linkedin-logo.svg")} alt="linkedin" title="linkedin"/>
+              </a>
+              <a href="https://github.com/brooklynbodega">
+                <img src={ require("../images/github-logo.svg")} alt="github" title="github"/>
+              </a>
+              <a href="mailto:raul.amontero@gmail.com">
+                <img src={ require("../images/gmail-logo.svg")} alt="gmail" title="gmail"/>
+              <a href="https://www.facebook.com/montero.raul">
+                <img src={ require("../images/facebook-logo.svg")} alt="facebook" title="facebook"/>
+              </a>
+              </a>
+            </div>
           </div>
         </div>
       </div>
